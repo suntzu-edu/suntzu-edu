ed1326a458fe0ce6d38d71c89d5cb5e500aaae03
Hang <suntzu_edu@protonmail.com>
2022-01-05 17:13:15 -0700

    1/5/22

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,7 +1,7 @@
 
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 DEBUG = True
 
@@ -12,5 +12,11 @@ app.config.from_object(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 
 app.run(host='0.0.0.0', port=5005)
