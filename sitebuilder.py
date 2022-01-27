779b5aff75cbc72e3246f4c37d289d29dcfb2365
Hang <suntzu_edu@protonmail.com>
2022-01-27 14:56:40 -0700

    before uploading to Github

--- sitebuilder.py
+++ sitebuilder.py
@@ -2,11 +2,15 @@
 import sys
 
 from flask import Flask, render_template, url_for
+from flask_frozen import Freezer
 
 DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+app.config['FREEZER_DESTINATION'] = './docs/'
+freezer = Freezer(app)
 
 
 @app.route('/')
@@ -25,6 +29,12 @@ def programs():
 def notes():
     return render_template('notes.html')
 
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'build':
+        freezer.freeze()
+    else:
+        app.run(host='0.0.0.0', port=5007)
 
 
 app.run(host='0.0.0.0', port=5005)
